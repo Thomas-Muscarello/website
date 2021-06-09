26ffaf6b2791dec317a3fd10b555277718d6ee63
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-06-08 21:36:28 -0400

    blog page

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,29 +1,32 @@
-import React, {useState, useEffect} from 'react'
-import BlogContainer from './blogContainer'
-//Blog Page
+import React, { useState, useEffect } from 'react';
+import BlogContainer from './blogContainer';
 
-const Blog = ()=>{
-    const rss2json = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40muscarello-thomas"
+const Blog = () => {
+    const rss2json = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40muscarello-thomas";
+    
     const [myBlog, setMyBlog] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch(rss2json)
-            .then(res=>res.json())
-            .then(data =>{
+            .then(res => res.json())
+            .then(data => {
                 setMyBlog(data)
-                console.log(data)
+                // console.log(data)
             })
     }, [rss2json]);
 
-    return(
-         <div id='blog'>
-            This is the Blog page
-            <BlogContainer/>
-            <BlogContainer/>
-            <BlogContainer/> 
+    function displayBlogs() {
+        console.log(myBlog)
+        return myBlog.items && myBlog.items.map(blog => {
+            return blog.categories.length > 0 && <BlogContainer key={blog.pubDate} blogData={blog} />
+        })
+    }
+
+    return (
+            <div className="BlogsContainer">
+                {displayBlogs()}
             </div>
-        )
-    
+    );
 }
 
-export default Blog
+export default Blog;
