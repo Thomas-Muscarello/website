17baca8edc14778b3df0af0b920fddac21219a54
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-06-17 14:05:24 -0400

    added social media links on contact

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -6,6 +6,24 @@ class Contact extends React.Component{
         return(
             <div id='contact'>
                 This is the Contact page
+                <ul>
+                    <a href="https://github.com/Thomas-Muscarello">
+                        <img src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="HTML tutorial" class="social_media">
+                        </img>
+                    </a>
+                    <a href="https://www.linkedin.com/in/tmuscarello/">
+                        <img src="https://pngimg.com/uploads/linkedIn/linkedIn_PNG38.png" alt="HTML tutorial" class="social_media">
+                        </img>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UC4OeBnWMMThkJI4yoNeZdXQ">
+                        <img src="https://i.pinimg.com/originals/09/f4/72/09f4726125ab5fa8cbcf754b9ba07e7c.jpg" alt="HTML tutorial" class="social_media">
+                        </img>
+                    </a>
+
+                </ul>
+                
+
+               
             </div>
         )
     }
