216a90108bffa1c2815b74fb02ef447614b6b1ed
Thomas Muscarello <muscarello.thomas@gmail.com>
2021-06-22 20:00:12 -0400

    about styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,6 @@ import React from 'react'
 const About = (props) =>{
     return(
          <div class="about">
-             <h1>Hey, I'm Tom!</h1>
              <h2> I have just finished The Flatiron School for Software Engineering. For the past 10 months I have had a hands on experience learning different langues in order to become a full stack engineer.</h2>
              <br/>
              <h2> I am proficient in the following:
